118e734734e5c1998cbd5c780f39904767fff783
xavier dupré <xavier.dupre@gmail.com>
2019-03-12 22:51:54 +0100

    rename parameter

--- onnx/bench_plot_onnxruntime_random_forest.py
+++ onnx/bench_plot_onnxruntime_random_forest.py
@@ -29,7 +29,7 @@ filename = os.path.splitext(os.path.split(__file__)[-1])[0]
 def run_bench(repeat=10, verbose=False):
 
     pbefore = dict(dim=[1, 5, 10, 20, 50, 100],
-                   max_depths=[2, 5, 10],
+                   max_depth=[2, 5, 10],
                    n_estimators=[1, 10, 100])
     pafter = dict(N=[1, 10])
 
