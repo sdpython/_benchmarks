7b5d5d45922582026ba816d0ff036d966332b604
xavier dupré <xavier.dupre@gmail.com>
2019-05-22 20:03:46 +0200

    Update bench_plot_gridsearch.py

--- scikit-learn/bench_plot_gridsearch.py
+++ scikit-learn/bench_plot_gridsearch.py
@@ -99,7 +99,7 @@ class PolyBenchPerfTest(BenchPerfTest):
 # +++++++++
 
 
-@ignore_warnings(category=(FutureWarning, UserWarning))
+@ignore_warnings(category=(FutureWarning, UserWarning, DeprecationWarning))
 def run_bench(repeat=10, verbose=False):
     pbefore = dict(dim=[5, 10])
     pafter = dict(N=[100, 1000, 10000], n_jobs=[1, 2, 3])
