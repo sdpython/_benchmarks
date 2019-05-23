59c82136b010c6e9337ecc30fefc9d62154a9b4b
xavier dupré <xavier.dupre@gmail.com>
2019-05-23 12:01:01 +0200

    Update bench_plot_gridsearch_cache.py

--- scikit-learn/bench_plot_gridsearch_cache.py
+++ scikit-learn/bench_plot_gridsearch_cache.py
@@ -101,8 +101,8 @@ class GridSearchBenchPerfTest(BenchPerfTest):
 
 @ignore_warnings(category=(FutureWarning, UserWarning, DeprecationWarning))
 def run_bench(repeat=3, verbose=False, number=1):
-    pbefore = dict(dim=[5, 10, 20, 50])
-    pafter = dict(N=[100, 1000, 10000, 100000], n_jobs=[1, 2, 3, 4])
+    pbefore = dict(dim=[5, 10, 20])
+    pafter = dict(N=[100, 1000, 10000], n_jobs=[1, 2, 3, 4])
 
     bp = BenchPerf(pbefore, pafter, GridSearchBenchPerfTest)
 
