13b0af2461e09980fcef603b0bc99a74feaa3b82
xavier dupré <xavier.dupre@gmail.com>
2019-07-31 19:59:27 +0200

    Update run_sklbenchmark.sh

--- sklbench/run_sklbenchmark.sh
+++ sklbench/run_sklbenchmark.sh
@@ -2,6 +2,9 @@ echo --CLONE--
 git clone https://github.com/sdpython/scikit-learn_benchmarks
 cd scikit-learn_benchmarks
 
+echo --MACHINE--
+asv machine --yes || exit 1
+
 echo --RUN-BENCHMARK--
 # asv run -b _bench --python=./_venv/bin/python
 asv run -b _bench || exit 1
