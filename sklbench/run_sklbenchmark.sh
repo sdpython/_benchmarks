c8103b391b7ab4648b787b46293478427f7d59da
xavier dupré <xavier.dupre@gmail.com>
2019-08-01 17:21:43 +0200

    update batch script

--- sklbench/run_sklbenchmark.sh
+++ sklbench/run_sklbenchmark.sh
@@ -19,12 +19,12 @@ asv run -b ElasticNet --append-samples || exit 1
 asv run -b GradientBoosting --append-samples --no-pull || exit 1
 asv run -b KMeans --append-samples --no-pull || exit 1
 asv run -b KNeighbors --append-samples --no-pull || exit 1
+asv run -b Lasso --append-samples --no-pull || exit 1
 asv run -b LinearRegression --append-samples --no-pull || exit 1
 asv run -b LogisticRegression --append-samples --no-pull || exit 1
 asv run -b PCA --append-samples --no-pull || exit 1
 asv run -b RandomForest --append-samples --no-pull || exit 1
 asv run -b Ridge --append-samples --no-pull || exit 1
-asv run -b Lasso --append-samples --no-pull || exit 1
 asv run -b SGDRegressor --append-samples --no-pull || exit 1
 asv run -b SVC --append-samples --no-pull || exit 1
 
