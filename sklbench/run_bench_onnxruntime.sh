ac05b365dd2823398b65bee7adb610ea93b7c1e1
xavier dupré <xavier.dupre@gmail.com>
2019-10-09 15:03:40 +0200

    add benchmark

---
+++ sklbench/run_bench_onnxruntime.sh
@@ -0,0 +1,13 @@
+echo --CLONE--
+git clone -b master --single-branch https://github.com/xadupre/scikit-onnx-benchmark.git --recursive
+cd scikit-onnx-benchmark
+git pull
+echo --BENCH--
+python3.7 -m asv run --show-stderr --config asv.conf.json
+if [ -d html ]
+then
+    echo --REMOVE HTML--
+    rm html -r -f
+fi
+echo --PUBLISH--
+python3.7 -m asv publish --config asv.conf.json -o html || exit 1
