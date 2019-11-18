5762e7c424d47af45c359aa1d15161aeeb0934b6
xavier dupré <xavier.dupre@gmail.com>
2019-11-18 17:49:00 +0100

    update benchmarks

---
+++ sklbench/run_bench_skl2onnx_cpp_same.sh
@@ -0,0 +1,31 @@
+echo --PIP--
+pip install git+https://github.com/sdpython/asv.git@jenkins
+pip install git+https://github.com/scikit-learn/scikit-learn.git
+
+echo --CLONE--
+if [ ! -d asv-skl2onnx ]
+then
+    echo --CLONE--
+    git clone https://github.com/sdpython/asv-skl2onnx.git --recursive
+else
+    echo --UPDATE--
+    cd asv-skl2onnx
+    git pull
+    git submodule update --init --recursive
+    cd ..
+fi
+
+cd asv-skl2onnx
+git pull
+echo --BENCH-CREATE--
+python3.7 -m mlprodict asv_bench --location . --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVMRegressor,SVMClassifier,RandomForestRegressor,RandomForestClassifier,DecisionTreeRegressor,DecisionTreeClassifier,AdaBoostClassifier,AdaBoostRegressor -v 1 || exit 1
+echo --BENCH-RUN--
+python3.7 -m asv run --show-stderr --config asv.conf.json --environment existing:same
+if [ -d html ]
+then
+    echo --REMOVE HTML--
+    rm html -r -f
+fi
+echo --PUBLISH--
+python3.7 -m asv publish --config asv.conf.json -o html || exit 1
+python3.7 -m mlprodict asv2csv -f results -o "asv_<date>.csv" --baseline skl || exit 1
