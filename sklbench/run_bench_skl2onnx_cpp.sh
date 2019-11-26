e3bc9c0bf58ddd1ae6852a13934cf0e82e6676b3
xavier dupré <xavier.dupre@gmail.com>
2019-11-27 00:43:55 +0100

    Update run_bench_skl2onnx_cpp.sh

--- sklbench/run_bench_skl2onnx_cpp.sh
+++ sklbench/run_bench_skl2onnx_cpp.sh
@@ -14,11 +14,11 @@ else
     git submodule update --init --recursive
     cd ..
 fi
-
+-n
 cd asv-skl2onnx
 git pull
 echo --BENCH-CREATE--
-python3.7 -m mlprodict asv_bench --location . --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVR,SVC,NuSVC,NuSVR,LinearSVC,LinearSVR,RandomForestRegressor,RandomForestClassifier,DecisionTreeRegressor,DecisionTreeClassifier,AdaBoostClassifier,AdaBoostRegressor -v 1 || exit 1
+python3.7 -m mlprodict asv_bench --location . -n 4,20,100 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVR,SVC,NuSVC,NuSVR,LinearSVC,LinearSVR,RandomForestRegressor,RandomForestClassifier,DecisionTreeRegressor,DecisionTreeClassifier,AdaBoostClassifier,AdaBoostRegressor -v 1 || exit 1
 echo --BENCH-RUN--
 python3.7 -m asv run --show-stderr --config asv.conf.json
 if [ -d html ]
