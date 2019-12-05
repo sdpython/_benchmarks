8d3f10d558b8ca459d8b8570dc7629417098b66e
xavier dupré <xavier.dupre@gmail.com>
2019-12-05 10:12:32 +0100

    Update run_profile_skl2onnx_classifier.sh

--- sklbench/run_profile_skl2onnx_classifier.sh
+++ sklbench/run_profile_skl2onnx_classifier.sh
@@ -2,27 +2,29 @@ echo --PIP--
 pip install git+https://github.com/sdpython/asv.git@jenkins
 pip install git+https://github.com/scikit-learn/scikit-learn.git
 
-echo --CLONE--
-if [ ! -d asv-skl2onnx ]
-then
-    echo --CLONE--
-    git clone https://github.com/sdpython/asv-skl2onnx.git --recursive
-else
-    echo --UPDATE--
-    cd asv-skl2onnx
-    git pull
-    git submodule update --init --recursive
-    cd ..
-fi
--n
-cd asv-skl2onnx
-git pull
+echo --CREATE-FOLDER--
+mkdir profiles
+cd profiles
+
 echo --BENCH-CREATE--
-python3.7 -m mlprodict asv_bench --location . -n 1,1000 --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVC,NuSVC,LinearSVC,RandomForestClassifier,DecisionTreeClassifier,AdaBoostClassifier,LogisticRegression,KNeighborsClassifier,MLPClassifier,MultinomialNB,BernoulliNB,OneVsRestClassifier -v 1 || exit 1
+python3.7 -m mlprodict asv_bench --location . -n 4,50 -d 1,1000 -o last --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVC,RandomForestClassifier,DecisionTreeClassifier,AdaBoostClassifier,LogisticRegression,KNeighborsClassifier,MLPClassifier,MultinomialNB,BernoulliNB,OneVsRestClassifier -v 1 || exit 1
+
 echo --PROFILE-RUN--
-if [ -d html ]
-then
-    echo --REMOVE HTML--
-    rm html -r -f
-fi
+
+# bash ./pyspy/ensemble/AdaBoostClassifier/
+
+bash ./pyspy/ensemble/RandomForestClassifier/bench_RandomForestClassifier_default_b_cl_1_4_12_float_.sh
+bash ./pyspy/ensemble/RandomForestClassifier/bench_RandomForestClassifier_default_b_cl_1_50_12_float_.sh
+bash ./pyspy/ensemble/RandomForestClassifier/bench_RandomForestClassifier_default_b_cl_1000_4_12_float_.sh
+bash ./pyspy/ensemble/RandomForestClassifier/bench_RandomForestClassifier_default_b_cl_1000_50_12_float_.sh
+
+# bash ./pyspy/linear_model/LogisticRegression/ nozipmap raw_score
+# bash ./pyspy/naive_bayes/BernoulliNB/
+# bash ./pyspy/naive_bayes/MultinomialNB/
+# bash ./pyspy/neighbors/KNeighborsClassifier/ cdist
+# bash ./pyspy/neural_network/MLPClassifier/
+# bash ./pyspy/svm/SVC/ linear poly rbf sigmoid
+# bash ./pyspy/tree/LinearSVC/DecisionTreeClassifier
+
 echo --PUBLISH--
+mkdir svg
