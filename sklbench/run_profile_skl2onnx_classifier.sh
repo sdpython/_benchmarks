8fb1e4eef09148eeb0fb9950bd4ae3c2fcebf8f1
xavier dupré <xavier.dupre@gmail.com>
2019-12-05 16:27:49 +0100

    Update run_profile_skl2onnx_classifier.sh

--- sklbench/run_profile_skl2onnx_classifier.sh
+++ sklbench/run_profile_skl2onnx_classifier.sh
@@ -3,18 +3,19 @@ mkdir profiles
 cd profiles
 
 echo --BENCH-CREATE--
-python -m mlprodict asv_bench --location . -n "4,50" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVC,RandomForestClassifier,DecisionTreeClassifier,AdaBoostClassifier,LogisticRegression,KNeighborsClassifier,MLPClassifier,MultinomialNB,BernoulliNB,OneVsRestClassifier -v 1 || exit 1
+python -m mlprodict asv_bench --location . -n "4" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVC,RandomForestClassifier,DecisionTreeClassifier,AdaBoostClassifier,LogisticRegression,KNeighborsClassifier,MLPClassifier,MultinomialNB,BernoulliNB,OneVsRestClassifier -v 1 || exit 1
 
 echo --PROFILE-RUN--
 
 # bash ./pyspy/ensemble/AdaBoostClassifier/
 
+echo --RandomForestClassifier--
 cd ./pyspy/ensemble/RandomForestClassifier
 export PYTHONPATH=../../../benches/ensemble/RandomForestClassifier
-bash ./bench_RandomForestClassifier_default_b_cl_1_4_12_float_.sh || exit 1
-bash ./bench_RandomForestClassifier_default_b_cl_1_50_12_float_.sh || exit 1
-bash ./bench_RandomForestClassifier_default_b_cl_1000_4_12_float_.sh || exit 1
-bash ./bench_RandomForestClassifier_default_b_cl_1000_50_12_float_.sh || exit 1
+for f in ./*.sh;
+echo -- $f --
+bash $f || exit 1;
+done
 cd ../../..
 
 # bash ./pyspy/linear_model/LogisticRegression/ nozipmap raw_score
