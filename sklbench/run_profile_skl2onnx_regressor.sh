078f524ae1c4fbb83ccf0f0b2db0aefa9f25e020
xavier dupré <xavier.dupre@gmail.com>
2019-12-06 18:32:55 +0100

    update benchmark

--- sklbench/run_profile_skl2onnx_regressor.sh
+++ sklbench/run_profile_skl2onnx_regressor.sh
@@ -3,9 +3,7 @@ mkdir profiles
 cd profiles
 
 echo --BENCH-CREATE--
-python -m mlprodict asv_bench --location . -n "4,50" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" 
---conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" 
---models SVR,RandomForestRegressor,DecisionTreeRegressor,AdaBoostRegressor,LinearRegression,KNeighborsRegressor,MLPRegressor -v 1 || exit 1
+python -m mlprodict asv_bench --location . -n "4,50" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVR,RandomForestRegressor,DecisionTreeRegressor,AdaBoostRegressor,LinearRegression,KNeighborsRegressor,MLPRegressor -v 1 || exit 1
 
 echo --PROFILE-RUN--
 
