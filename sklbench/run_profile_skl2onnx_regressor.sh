1c20e480564769422e02607fae757d97166edf78
xavier dupré <xavier.dupre@gmail.com>
2019-12-09 18:00:46 +0100

    update benchmark

--- sklbench/run_profile_skl2onnx_regressor.sh
+++ sklbench/run_profile_skl2onnx_regressor.sh
@@ -3,7 +3,7 @@ mkdir profiles
 cd profiles
 
 echo --BENCH-CREATE--
-python -m mlprodict asv_bench --location . -n "4,50" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python_compiled,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVR,RandomForestRegressor,DecisionTreeRegressor,AdaBoostRegressor,LinearRegression,KNeighborsRegressor,MLPRegressor -v 1 || exit 1
+python -m mlprodict asv_bench --location . -n "4,50" -d "1,1000" -o -1 --add_pyspy 1 --runtime "scikit-learn,python_compiled,onnxruntime1" --conf_params "project,asv-skl2onnx;project_url,https://github.com/sdpython/asv-skl2onnx" --models SVR,RandomForestRegressor,DecisionTreeRegressor,AdaBoostRegressor,LinearRegression,KNeighborsRegressor,MLPRegressor,HistGradientBoostingRegressor -v 1 || exit 1
 
 echo --PROFILE-RUN--
 
@@ -23,6 +23,19 @@ cd ../../..
 echo --RandomForestRegressor--
 cd ./pyspy/ensemble/RandomForestRegressor
 export PYTHONPATH=../../../benches/ensemble/RandomForestRegressor
+for f in ./*float*.sh
+do
+    if [[ $f != *64* ]]
+    then
+        echo "run '$f'"
+        bash $f
+    fi
+done
+cd ../../..
+
+echo --HistGradientBoostingRegressor--
+cd ./pyspy/ensemble/HistGradientBoostingRegressor
+export PYTHONPATH=../../../benches/ensemble/HistGradientBoostingRegressor
 for f in ./*float*.sh
 do
     if [[ $f != *64* ]]
