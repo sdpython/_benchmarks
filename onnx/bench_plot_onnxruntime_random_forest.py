7d4125446b7e7764c3f60d8d0a60b781b559162a
xavier dupré <xavier.dupre@gmail.com>
2019-12-21 17:06:59 +0100

    Update bench_plot_onnxruntime_random_forest.py

--- onnx/bench_plot_onnxruntime_random_forest.py
+++ onnx/bench_plot_onnxruntime_random_forest.py
@@ -32,7 +32,7 @@ def run_bench(repeat=10, verbose=False):
     pbefore = dict(dim=[1, 5, 10, 20, 50, 100],
                    max_depth=[2, 5, 10],
                    n_estimators=[1, 10, 100, 1000, 10000],
-                   onnx_options=[None, {RandomForestClassifier: {'zipmap': False}}])
+                   onnx_options=[None])
     pafter = dict(N=[1, 10, 100])
 
     test = lambda dim=None, **opts: OnnxRuntimeBenchPerfTestBinaryClassification(
