e8852459056408bbf7680dde58f25e1219f8a577
xavier dupré <xavier.dupre@gmail.com>
2019-12-21 01:54:19 +0100

    update becnhmarks

--- onnx/bench_plot_onnxruntime_decision_tree.py
+++ onnx/bench_plot_onnxruntime_decision_tree.py
@@ -31,7 +31,7 @@ def run_bench(repeat=10, verbose=False):
 
     pbefore = dict(dim=[1, 5, 10, 20, 50, 100, 200],
                    max_depth=[3, 10, 20])
-    pafter = dict(N=[1, 10, 100, 1000])
+    pafter = dict(N=[1, 10, 100, 1000, 10000])
 
     test = lambda dim=None, **opts: OnnxRuntimeBenchPerfTestBinaryClassification(
         DecisionTreeClassifier, dim=dim, **opts)
