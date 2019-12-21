e87992c2c06d5457fa84fed638dfcef76fea18e4
xavier dupré <xavier.dupre@gmail.com>
2019-12-21 11:47:11 +0100

    fix benchmarks

--- onnx/bench_plot_onnxruntime_gbr.py
+++ onnx/bench_plot_onnxruntime_gbr.py
@@ -29,9 +29,9 @@ filename = os.path.splitext(os.path.split(__file__)[-1])[0]
 @ignore_warnings(category=FutureWarning)
 def run_bench(repeat=10, verbose=False):
 
-    pbefore = dict(dim=[1, 5, 10, 20, 50, 100, 1000, 10000],
+    pbefore = dict(dim=[1, 5, 10, 20, 50, 100],
                    max_depth=[2, 5, 10],
-                   n_estimators=[1, 10, 100],
+                   n_estimators=[1, 10, 100, 1000, 10000],
                    onnx_options=[None])
     pafter = dict(N=[1, 10, 100])
 
