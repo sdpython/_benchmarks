c988fefbbf70b630c3c7879146df0a77cc67a74c
xavier dupré <xavier.dupre@gmail.com>
2020-12-03 18:30:48 +0100

    update scripts

---
+++ skl2onnx/run.sh
@@ -0,0 +1,14 @@
+cd skl2onnx
+echo --BENCHMARK1--
+python -u bench_plot_onnxruntime_random_forest_reg.py || exit 1
+echo --BENCHMARK2--
+python -u bench_plot_onnxruntime_svm_reg.py || exit 1
+echo --BENCHMARK3--
+python -u bench_plot_onnxruntime_logreg.py || exit 1
+echo --BENCHMARK4--
+python -u bench_plot_onnxruntime_linreg.py || exit 1
+echo --BENCHMARK-GRAPH--
+cd results
+python -u post_graph.py || exit 1
+cd ../..
+
