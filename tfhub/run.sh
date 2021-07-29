42dfcdcb8222256c03e4aca1a7271bcb33c2455f
xavier dupré <xavier.dupre@gmail.com>
2021-07-29 11:35:18 +0200

    Update run.sh

--- tfhub/run.sh
+++ tfhub/run.sh
@@ -1,8 +1,8 @@
 echo --INSTALL--
-python -m pip install tensorflow tensorflow_hub
+python -m pip install tensorflow tensorflow_hub --no-deps
 
 echo --RUN--
 cd tfhub
-python tfhub_mobilenet_v3_small_075_224.py
+python -u tfhub_mobilenet_v3_small_075_224.py
 cd ..
 
